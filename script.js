1dd0ff75f3b32b198c16eb500a14eb1cdcfc34f8
Brendon Chow <brendonchow123@gmail.com>
2022-11-30 11:16:28 +1100

    Implement dynamic display

--- script.js
+++ script.js
@@ -29,3 +29,21 @@ function operate(operator, x, y) {
       return divide(x, y);
   }
 }
+const displayCurrent = document.querySelector(".displayCurrent")
+const operands = document.querySelectorAll(".operand");
+const expressions = []
+const firstNumber = document.querySelector(".first")
+
+operands.forEach(button => {
+  
+  button.addEventListener("click", e => {
+    let buttonNumber = e.target.textContent;
+    if (displayCurrent.textContent == 0) firstNumber.textContent = buttonNumber;
+    else {
+      const newNumber = document.createElement("span");
+      newNumber.textContent = e.target.textContent;
+      displayCurrent.appendChild(newNumber);
+    }
+    expressions.push(parseInt(buttonNumber));
+  })
+});
