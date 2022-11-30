a669ab08284ffa720b2b0b1748f4e367068e6667
Brendon Chow <brendonchow123@gmail.com>
2022-11-30 18:34:27 +1100

    Fix bugs

--- script.js
+++ script.js
@@ -3,6 +3,104 @@ const operands = document.querySelectorAll(".operand");
 const displayPrev = document.querySelector(".displayPrev");
 const operators = document.querySelectorAll(".operator");
 const backspace = document.querySelector(".backspace");
+const clear = document.querySelector(".clear");
+const equal = document.querySelector(".equal");
+
+equal.addEventListener("mousedown", inputEqual)
+clear.addEventListener("click", inputClear);
+backspace.addEventListener("mousedown", inputBackspace);
+
+operands.forEach(button => button.addEventListener("mousedown", e => inputOperand(e.target.textContent)));
+
+operators.forEach(button => {
+  button.addEventListener("mousedown", e => inputOperator(e.target.textContent));
+})
+
+window.addEventListener("keydown", event => {
+  const button = document.querySelector(`[data-key="${event.key}"]`);
+  if (button) choice[button.classList.value](button.textContent);
+});
+
+let prevNumber = "";
+let operator = "";
+let currentNumber = "";
+
+function inputOperator(value) {
+  if (divideByZero(operator, currentNumber)) return;
+  else if (currentNumber && prevNumber) {
+    displayCurrent.textContent = Math.round(operate(prevNumber, operator, currentNumber) * 1000) / 1000;
+  }
+  operator = value;
+  prevNumber = displayCurrent.textContent;
+  currentNumber = "";
+  displayPrev.textContent = `${prevNumber} ${operator}`; 
+}
+
+function inputEqual() {
+  if (operator && prevNumber && currentNumber) {
+    if (divideByZero(operator, currentNumber)) return
+    displayPrev.textContent = `${prevNumber} ${operator} ${currentNumber} =`;
+    displayCurrent.textContent = Math.round(operate(prevNumber, operator, currentNumber) * 1000) / 1000;
+    currentNumber = displayCurrent.textContent;
+    prevNumber = ""
+    operator = ""
+  }
+}
+
+function divideByZero(operator, currentNumber) {
+  if (operator === "/" && currentNumber === "0") {
+    alert("DIVIDING BY ZERO");
+    return true;
+  }
+}
+
+function inputOperand(value) {
+  if (value === "." && currentNumber.includes(".")) return;
+  else if (currentNumber === "" || currentNumber === "0") {
+    if (value !== ".") {
+      displayCurrent.textContent = value;    
+    } else {
+      displayCurrent.textContent = "0.";
+    }
+    currentNumber = displayCurrent.textContent;
+  } else {
+    displayCurrent.textContent += value;
+    currentNumber += value;
+    }
+}
+
+function inputClear() {
+  displayCurrent.textContent = "0";
+  displayPrev.textContent = "";
+  currentNumber = "";
+  prevNumber = "";
+  operator = "";
+}
+
+function inputBackspace() {
+  if (displayCurrent.textContent.length > 0) {
+    displayCurrent.textContent = displayCurrent.textContent.substr(0, displayCurrent.textContent.length - 1);
+    currentNumber = currentNumber.substr(0, currentNumber.length - 1);
+  }
+}
+
+const choice = {
+  operand(value) {
+    inputOperand(value);
+  },
+  operator(value) {
+    inputOperator(value);
+  },
+  backspace() {
+    inputBackspace();
+  },
+  equal() {
+    inputEqual();
+  },
+  clear() {
+    inputClear();
+  }
+}
 
 function add(x, y) {
   return x + y;
@@ -37,94 +135,4 @@ function operate(x, operator, y) {
       return divide(x, y);
   }
 }
-
-let prevNumber = "";
-let operator = "";
-
-operators.forEach(button => {
-  button.addEventListener("mousedown", e => inputOperator(e.target.textContent));
-})
-function inputOperator(value) {
-  if (!prevNumber) {
-    operator = value;
-  } else {
-    reset(value);
-  }
-  displayPrev.textContent = `${displayCurrent.textContent} ${operator}`; 
-}
-
-const equal = document.querySelector(".equal");
-equal.addEventListener("mousedown", inputEqual)
-function inputEqual() {
-  if (operator && prevNumber) {
-    displayPrev.textContent = `${prevNumber} ${operator} ${displayCurrent.textContent} =`;
-    reset("");
-  }
-}
-
-function reset(operatorVal) {
-  displayCurrent.textContent = Math.round(operate(prevNumber, operator, displayCurrent.textContent) * 1000) / 1000;
-  prevNumber = "";
-  operator = operatorVal;
-}
-
-operands.forEach(button => button.addEventListener("mousedown", e => inputOperand(e.target.textContent)));
-function inputOperand(value) {
-  if (operator && !prevNumber) {
-    prevNumber = displayCurrent.textContent;
-    displayCurrent.textContent = "0";
-  }    
-  if (displayCurrent.textContent === "0" && value !== ".") {
-    if (value !== "0") {
-      displayCurrent.textContent = value;
-    }
-  } 
-  else {
-    if (value === ".") { 
-      if (displayCurrent.textContent.includes(".")) return;
-    }
-    displayCurrent.textContent += value;
-  }
-}
-
-const clear = document.querySelector(".clear");
-clear.addEventListener("click", inputClear);
-function inputClear() {
-  displayCurrent.textContent = "0";
-  displayPrev.textContent = "";
-  prevNumber = "";
-  operator = "";
-}
-
-backspace.addEventListener("mousedown", () => inputBackspace);
-function inputBackspace() {
-  let currentNumber = displayCurrent.textContent
-  if (currentNumber !== "0") {
-    if (currentNumber.length === 1) displayCurrent.textContent = "0";
-    else displayCurrent.textContent = currentNumber.slice(0, currentNumber.length - 1);
-  }
-}
-
-const choice = {
-  operand(value) {
-    inputOperand(value);
-  },
-  operator(value) {
-    inputOperator(value);
-  },
-  backspace() {
-    inputBackspace();
-  },
-  equal() {
-    inputEqual();
-  },
-  clear() {
-    inputClear();
-  }
-}
-
-window.addEventListener("keydown", event => {
-  const button = document.querySelector(`[data-key="${event.key}"]`);
-  if (button) choice[button.classList.value](button.textContent);
-});
   
